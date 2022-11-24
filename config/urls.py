37014fd156b99bd8d19ad416337877fc4c11967f
OQdevpy <117621709+OQdevpy@users.noreply.github.com>
2022-11-24 17:25:26 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.template.context_processors import static
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,7 @@ urlpatterns = [
     path('davomat/',include('davomat.urls')),
                 #   path('contact/',include('contact.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
